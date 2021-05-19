13de28502f5517798ccf17c7e6db9976ec13f797
Aniket-Dilip-Panse <84241376+Aniket-Dilip-Panse@users.noreply.github.com>
2021-05-19 20:34:57 +0530

    Update mahadev.py

--- Turtle/mahadev.py
+++ Turtle/mahadev.py
@@ -1,26 +1,31 @@
 import turtle as turtle
 turtle.bgcolor("black")   # background color of window.
 turtle.color("#ff8000")   # pencolor used for drawing.
-turtle.speed(0)           # slow motion.
+turtle.speed(1)           # slow motion.
 turtle.pensize(3)         # size of pen
 
 turtle.penup()
-turtle.goto(0,-130)
+turtle.goto(100,-130)
 turtle.pendown()
 # chandrakor code.
-turtle.circle(120,-80,45)
-turtle.circle(120,160,45)
-turtle.circle(119,-80,50)
-turtle.circle(119,-80,50)
+turtle.begin_fill()
+turtle.color("#ff8000")
+turtle.right(90)
+turtle.circle(-100,180)
+turtle.up()
+turtle.fd(10)
+turtle.down()
+turtle.circle(-100,-180)
+turtle.end_fill()
 
 turtle.penup()
-turtle.goto(0,0)
+turtle.goto(0,-50)
 turtle.pendown()
 
 # first shapes
 turtle.color("#ff8000")
 turtle.begin_fill()
-turtle.left(80)
+turtle.left(90)
 turtle.fd(100)
 turtle.circle(25,180)
 turtle.fd(200)
@@ -33,7 +38,7 @@ turtle.circle(25)
 turtle.end_fill()
 
 turtle.penup()
-turtle.goto(0,70)
+turtle.goto(0,30)
 turtle.pendown()
 
 # second shapes
@@ -57,3 +62,4 @@ turtle.write("mera bhola hai bhandari",font=('arial',25))
 
 turtle.hideturtle()
 turtle.mainloop()
+
